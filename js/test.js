6decb6f6c741d27cc10be3a38ee6aed64c3c244d
Yaroslav Pendo <yaroslavpendo@Yaroslavs-MacBook-Air.local>
2024-04-16 02:49:44 +0300

    Js_Learn_V1.0

---
+++ js/test.js
@@ -0,0 +1,17 @@
+"use strict";
+
+console.log(typeof (4 + " - object"));
+
+
+console.log(Number("5") + 5);
+
+
+let incr = 10,
+    decro = 20;
+
+
+// let res = Boolean(true^true);
+
+console.log(res);
+
+
